ce3e168819592a3d9d368f2835b1c97aadc85712
Samarth Jain <samarth16022002@gmail.com>
2021-06-02 11:30:59 +0530

    Reading data from csv file in assets folder, and printing on console

--- app/src/main/java/com/example/datavisualiser/MainActivity.java
+++ app/src/main/java/com/example/datavisualiser/MainActivity.java
@@ -2,7 +2,20 @@ package com.example.datavisualiser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
+
+import com.opencsv.CSVReaderHeaderAware;
+import com.example.datavisualiser.R;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            AssetFileDescriptor descriptor = getAssets().openFd("data_accidental_deaths.csv");
+            Map<String, String> var = new CSVReaderHeaderAware(new FileReader(descriptor.getFileDescriptor())).readMap();
+            for(Map.Entry<String,String> entry : var.entrySet()){
+                System.out.println(entry.getKey()+ " "+ entry.getValue()+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 }
